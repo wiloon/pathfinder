07be6c9891ba865da1ef9801399832681e7ae65d
Wang Yue <wiloon.wy@gmail.com>
2026-04-17 11:33:20 +0800

    ci

--- pathfinder-api/main.go
+++ pathfinder-api/main.go
@@ -81,8 +81,12 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Recovery())
 	r.Use(middleware.Logger())
+	allowOrigins := []string{"http://localhost:3000"}
+	if cfg.App.FrontendBaseURL != "" {
+		allowOrigins = append(allowOrigins, cfg.App.FrontendBaseURL)
+	}
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     allowOrigins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
